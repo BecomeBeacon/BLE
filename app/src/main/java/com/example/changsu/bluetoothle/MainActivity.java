7ccc85a7f27dd0ff285d872cb9b0ded563e86589
wangnan940624@gmail.com <wangnan940624@gmmail.com>
2017-04-09 07:14:55 +0900

    Slide

--- app/src/main/java/com/example/changsu/bluetoothle/MainActivity.java
+++ app/src/main/java/com/example/changsu/bluetoothle/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.changsu.bluetoothle;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.Toast;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     Button mBtnScan;
     Button mBtnMap;
@@ -25,6 +32,21 @@ public class MainActivity extends ActionBarActivity {
         mBtnMap = (Button)findViewById(R.id.btn_map);
         mBtnMap.setOnClickListener(mClickListener);
 
+
+        /* Slide */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*Slide Control*/
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        /*NavigationView*/
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
@@ -72,4 +94,38 @@ public class MainActivity extends ActionBarActivity {
             }
         }
     };
+
+
+
+    /*Slide Back*/
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_machine) {
+
+        } else if (id == R.id.nav_laf) {
+
+        } else if (id == R.id.nav_clock) {
+
+        } else if (id == R.id.nav_setting) {
+
+        }
+
+        /*Side Close*/
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
